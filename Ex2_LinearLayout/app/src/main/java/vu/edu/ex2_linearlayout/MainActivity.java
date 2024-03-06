d9c65af403c5a04f17ff056fdefa8fb90347ea94
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-06 09:12:20 +0700

    Thực hành tại phòng máy

---
+++ Ex2_LinearLayout/app/src/main/java/vu/edu/ex2_linearlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package vu.edu.ex2_linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //Nạp Layout
+    }
+}
