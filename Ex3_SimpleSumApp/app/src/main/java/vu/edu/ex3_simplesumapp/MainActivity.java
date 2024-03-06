f58f9d885eac4de03ac08e0292100a6a6d6a69f0
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-06 17:50:23 +0700

    Update 06/03/24-TH_NDN-205
    Ứng dụng tính tổng 2 số - Code lắng nghe và xử lý sự kiện OnClick: Xử lý backend: Bắt/Đáp ứng sự kiện bằng hàm xử lý cài đặt cho OnClick trên XML

--- Ex3_SimpleSumApp/app/src/main/java/vu/edu/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/vu/edu/ex3_simplesumapp/MainActivity.java
@@ -3,12 +3,37 @@ package vu.edu.ex3_simplesumapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Gắn Layout tương ứng với file này
         setContentView(R.layout.activity_main);
+    }
+
+    //Bộ lắng nghe và xử lý sự kiên click lên nút tính tổng
+    public void XuLyCong(View view){
+        //Tìm, tham chiếu đến điều khiển trên tệp XML, mapping sang Java file
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKQ);
+        //lấy dữ liệu về ở điều khiển số a
+        String strA = editTextSoA.getText().toString(); //strA="2"
+
+        //lấy dữ liệu về ở điều khiển số b
+        String strB = editTextSoB.getText().toString(); //strB="4"
+
+        //Chuyển dữ liệu sang dạng số
+        int so_A = Integer.parseInt(strA);  //2
+        int so_B = Integer.parseInt(strB);  //4
+        //Tính toán theo yêu cầu
+        int tong = so_A+so_B;   //6
+        String strTong = String.valueOf(tong); //chuyển sang dạng chuỗi; "6"
+        //Hiển thị ra màn hình
+        editTextKQ.setText(strTong);
     }
 }
