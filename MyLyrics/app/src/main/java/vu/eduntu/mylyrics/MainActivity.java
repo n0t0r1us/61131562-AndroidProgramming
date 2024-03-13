afb69c6056f2954cf737b9b987d9c5700e00067c
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-13 19:52:13 +0700

    Update Ex

--- MyLyrics/app/src/main/java/vu/eduntu/mylyrics/MainActivity.java
+++ MyLyrics/app/src/main/java/vu/eduntu/mylyrics/MainActivity.java
@@ -9,26 +9,28 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> dsBH;
+    ArrayAdapter<String> adapterBH;
+    ListView lvBaiHat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        timDK();
         //Bước 2
-        ArrayList<String> dsBH;
         dsBH = new ArrayList<String>();
         dsBH.add("Hotel California");
         dsBH.add("Careless Whisper");
         dsBH.add("Rasputin");
         //Bước 3
-        ArrayAdapter<String> adapterBH;
         adapterBH = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, dsBH);
         //Bước 4
-        ListView lvBaiHat = findViewById(R.id.lvLoiBaiHatYeuThich);
         lvBaiHat.setAdapter(adapterBH);
         //Bước 5 xử lý sự kiện
-        //
+    }
+    void timDK(){
+        lvBaiHat = findViewById(R.id.lvLoiBaiHatYeuThich);
     }
 }
