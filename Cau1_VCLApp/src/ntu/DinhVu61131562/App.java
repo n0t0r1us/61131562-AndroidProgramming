ac73e8a8daae5ebc1fc4f8cc4c4c416d0fada4b1
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-24 11:54:07 +0700

    1.2 Update & Khởi tạo Class CommonConfigs
    Chứa các hằng số được sử dụng để cấu hình giao diện người dùng của ứng dụng.

--- Cau1_VCLApp/src/ntu/DinhVu61131562/App.java
+++ Cau1_VCLApp/src/ntu/DinhVu61131562/App.java
@@ -1,5 +1,18 @@
 package ntu.DinhVu61131562;
 
+import javax.swing.SwingUtilities;
+
 public class App {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				new VCLGui().setVisible(true);
+				
+			}
+		});
+	}
 
 }
---
+++ Cau1_VCLApp/src/ntu/DinhVu61131562/CommonConfigs.java
@@ -0,0 +1,7 @@
+package ntu.DinhVu61131562;
+
+import java.awt.*;
+public class CommonConfigs {
+	public static final Dimension GUI_SIZE = new Dimension(540, 660);
+
+}
