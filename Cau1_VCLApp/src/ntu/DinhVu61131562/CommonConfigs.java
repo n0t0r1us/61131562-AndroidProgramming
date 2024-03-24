a81ff0c91640b6dd37a66a68f56a88602c727f34
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-24 12:34:50 +0700

    1.3 Khởi tạo 1 JLabel Banner Text

--- Cau1_VCLApp/src/ntu/DinhVu61131562/CommonConfigs.java
+++ Cau1_VCLApp/src/ntu/DinhVu61131562/CommonConfigs.java
@@ -2,6 +2,9 @@ package ntu.DinhVu61131562;
 
 import java.awt.*;
 public class CommonConfigs {
+	
 	public static final Dimension GUI_SIZE = new Dimension(540, 660);
+	
+	public static final Dimension BANNER_SIZE = new Dimension(GUI_SIZE.width, 50);	
 
 }
--- Cau1_VCLApp/src/ntu/DinhVu61131562/VCLGui.java
+++ Cau1_VCLApp/src/ntu/DinhVu61131562/VCLGui.java
@@ -1,6 +1,7 @@
 package ntu.DinhVu61131562;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class VCLGui extends JFrame {
 	/**
@@ -15,6 +16,17 @@ public class VCLGui extends JFrame {
 		setResizable(false);
 		setLayout(null);
 		
+		addGuiComponents();
+		
 	}
-
+	
+	private void addGuiComponents() {
+		JLabel bannerLabel = new JLabel("DANH SÁCH CÁC VIỆC CẦN LÀM");
+		bannerLabel.setBounds(
+				(CommonConfigs.GUI_SIZE.width - bannerLabel.getPreferredSize().width)/2,
+					15,
+				CommonConfigs.BANNER_SIZE.width,
+				CommonConfigs.BANNER_SIZE.height);
+		
+	}
 }
