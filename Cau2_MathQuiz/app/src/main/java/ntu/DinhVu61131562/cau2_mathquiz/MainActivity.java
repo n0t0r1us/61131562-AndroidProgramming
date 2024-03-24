a27a8e27fe1e9ab71ded51d071e92d88f1cc901b
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-24 20:05:45 +0700

    1. Thiết kế giao diện Nền Chuyển Tiếp

---
+++ Cau2_MathQuiz/app/src/main/java/ntu/DinhVu61131562/cau2_mathquiz/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu.DinhVu61131562.cau2_mathquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Cau2_MathQuiz/app/src/test/java/ntu/DinhVu61131562/cau2_mathquiz/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ntu.DinhVu61131562.cau2_mathquiz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
