ea5c6a94cc4391a0553988bd88843c1b22613bbe
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-25 14:52:43 +0700

    2.10 Khai báo biến thành phần UI và các biến khác
    Khai báo các biến để tham chiếu đến các thành phần UI như TextView và Button, soCauDung và tongSoCauHoi để theo dõi số câu trả lời đúng và tổng số câu hỏi.

--- Cau2_MathQuiz/app/src/main/java/ntu/DinhVu61131562/cau2_mathquiz/MainActivity.java
+++ Cau2_MathQuiz/app/src/main/java/ntu/DinhVu61131562/cau2_mathquiz/MainActivity.java
@@ -3,13 +3,24 @@ package ntu.DinhVu61131562.cau2_mathquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView cauHoiTextView;
+    private TextView ketQuaTextView;
+    private Button dapAn1Button, dapAn2Button, dapAn3Button, dapAn4Button;
+    private Button doiCauHoiButton;
+    private int soCauDung = 0;
+    private int tongSoCauHoi = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
     }
 }
