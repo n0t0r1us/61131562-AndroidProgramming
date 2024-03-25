0d89a673d81f1193469e279f2ec15d238919424b
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-25 15:16:29 +0700

    2.11 Khởi tạo các view từ layout
    Đổi lại ImageView của nút đổi câu hỏi thành ImageButton

--- Cau2_MathQuiz/app/src/main/java/ntu/DinhVu61131562/cau2_mathquiz/MainActivity.java
+++ Cau2_MathQuiz/app/src/main/java/ntu/DinhVu61131562/cau2_mathquiz/MainActivity.java
@@ -4,23 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView cauHoiTextView;
     private TextView ketQuaTextView;
     private Button dapAn1Button, dapAn2Button, dapAn3Button, dapAn4Button;
-    private Button doiCauHoiButton;
+    private ImageButton doiCauHoiButton;
     private int soCauDung = 0;
     private int tongSoCauHoi = 0;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        cauHoiTextView = findViewById(R.id.cauHoiTextView);
+        dapAn1Button = findViewById(R.id.dapAn1Button);
+        dapAn2Button = findViewById(R.id.dapAn2Button);
+        dapAn3Button = findViewById(R.id.dapAn3Button);
+        dapAn4Button = findViewById(R.id.dapAn4Button);
+        ketQuaTextView = findViewById(R.id.ketQuaTextView);
+        doiCauHoiButton = findViewById(R.id.doiCauHoiButton);
 
     }
 }
