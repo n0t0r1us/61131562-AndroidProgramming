01bf148dde0d44dce2f5ff58bf75a3046d4a83ca
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-04-10 18:07:47 +0700

    2. Onclick, Layout, Cardview Item
    Sử dụng RecyclerView để hiển thị danh sách các mục dữ liệu. Mỗi mục dữ liệu là một đối tượng gồm 2 thông tin (ảnh và caption của ảnh). Tập trung vào: Xử lý sự kiện click lên mục dữ liệu, và tút lại trình bày của mục dữ liệu sử dụng thêm Cardview. Đồng thời sử dụng các loại layout khác nhau của Recyclervew

--- UsingRecyclerView/app/src/main/java/ntu/dinhvu61131562/usingrecyclerview/LandScapeAdapter.java
+++ UsingRecyclerView/app/src/main/java/ntu/dinhvu61131562/usingrecyclerview/LandScapeAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -51,7 +52,7 @@ public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.Item
         return lstData.size();
     }
 
-    class ItemLandHolder extends RecyclerView.ViewHolder{
+    class ItemLandHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView tvCaption;
         ImageView ivLandscape;
 
@@ -59,6 +60,20 @@ public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.Item
             super(itemView);
             tvCaption = itemView.findViewById(R.id.textViewCaption);
             ivLandscape = itemView.findViewById(R.id.imageViewLand);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int viTriDuocClick = getAdapterPosition();
+            LandScape phanTuDuocClick = lstData.get(viTriDuocClick);
+            //pick thông tin
+            String ten = phanTuDuocClick.getLandCaption();
+            String tenFile = phanTuDuocClick.getLandImageFileName();
+            //Toast Tên
+            String chuoiThongBao = "Bạn vừa click vào: " + ten;
+            Toast.makeText(v.getContext(), chuoiThongBao, Toast.LENGTH_SHORT).show();
+
         }
     }
 }
--- UsingRecyclerView/app/src/main/java/ntu/dinhvu61131562/usingrecyclerview/MainActivity.java
+++ UsingRecyclerView/app/src/main/java/ntu/dinhvu61131562/usingrecyclerview/MainActivity.java
@@ -1,6 +1,7 @@
 package ntu.dinhvu61131562.usingrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,8 +23,12 @@ public class MainActivity extends AppCompatActivity {
         //4
         recyclerViewLandscape = findViewById(R.id.recyclerLand);
         //5
-        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
-        recyclerViewLandscape.setLayoutManager(layoutLinear);
+//        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+//        recyclerViewLandscape.setLayoutManager(layoutLinear);
+//        RecyclerView.LayoutManager layoutLinearHorizonal = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+//        recyclerViewLandscape.setLayoutManager(layoutLinearHorizonal);
+        RecyclerView.LayoutManager layoutGrid = new GridLayoutManager(this, 2);
+        recyclerViewLandscape.setLayoutManager(layoutGrid);
         //6
         landScapeAdapter = new LandScapeAdapter(this, recyclerViewDatas);
         //7
