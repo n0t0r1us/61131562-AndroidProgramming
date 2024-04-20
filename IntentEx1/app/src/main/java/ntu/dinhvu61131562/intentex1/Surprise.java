ef2888f18e1043390e8d299140df739e386d049e
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-04-20 17:00:16 +0700

    Xây dựng bộ lắng nghe, xử lý theo 1 cách khác
    1. Tìm đối tượng cần gắn bộ lắng nghe.
    2. Gắn bộ lắng nghe.
    3. Tạo thư.
    4. Gửi đi.

--- IntentEx1/app/src/main/java/ntu/dinhvu61131562/intentex1/Surprise.java
+++ IntentEx1/app/src/main/java/ntu/dinhvu61131562/intentex1/Surprise.java
@@ -2,7 +2,10 @@ package ntu.dinhvu61131562.intentex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Surprise extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Surprise extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_surprise);
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(ClickListenerChange);
     }
+
+    View.OnClickListener ClickListenerChange = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent backToHome = new Intent(Surprise.this, MainActivity.class);
+            startActivity(backToHome);
+        }
+    };
 }
