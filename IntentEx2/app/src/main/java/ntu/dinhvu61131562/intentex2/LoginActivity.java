d2f87174297f4a9b69cdaed7bf2235f24e737e77
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-04-21 00:57:13 +0700

    Fix lỗi ko hiện ảnh và code xử lý ở màn hình Login
    Xử lý đăng nhập, lấy dữ liệu:
    1. Tìm tham chiếu đến đk.
    2. Lấy dữ liệu.
    3. Kiểm tra mật khẩu, email.
    4. Nếu mk, email đúng chuyển sang màn hình Home, nếu sai thông báo lỗi.
    5. Gói dữ liệu vào iQuiz, dạng key - value; key đc dùng để bên kia lọc ra dữ liệu.
    6. Gửi đi.

--- IntentEx2/app/src/main/java/ntu/dinhvu61131562/intentex2/LoginActivity.java
+++ IntentEx2/app/src/main/java/ntu/dinhvu61131562/intentex2/LoginActivity.java
@@ -2,7 +2,12 @@ package ntu.dinhvu61131562.intentex2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,40 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText edtUserName = (EditText) findViewById(R.id.edtUserName);
+        EditText edtPassWord = (EditText) findViewById(R.id.edtPass);
+        EditText edtUserMail = (EditText) findViewById(R.id.edtMail);
+        Button btnXacNhan = (Button) findViewById(R.id.btnOK);
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edtUserName = (EditText) findViewById(R.id.edtUserName);
+                EditText edtPassWord = (EditText) findViewById(R.id.edtPass);
+                EditText edtUserMail = (EditText) findViewById(R.id.edtMail);
+                String tenDN = edtUserName.getText().toString();
+                String mk = edtPassWord.getText().toString();
+                String email = edtUserMail.getText().toString();
+                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+                boolean isValidEmail = email.matches(emailPattern);
+
+                if( tenDN.equals("dinhdu")&& mk.equals("6969") &&isValidEmail ){
+                    Intent iQuiz = new Intent(LoginActivity.this, HomeActivity.class);
+                    iQuiz.putExtra("ten_dang_nhap", tenDN);
+                    iQuiz.putExtra("mk",mk);
+                    iQuiz.putExtra("email", email);
+                    startActivity(iQuiz);
+                }
+                else {
+                    if(!isValidEmail){
+                        Toast.makeText(LoginActivity.this,"EMAIL KHÔNG HỢP LỆ!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "BẠN NHẬP SAI THÔNG TIN", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }
+
+            }
+        });
     }
 }
