b58aa6b105e252ca0d2eeee10e85d79fb4daa0bf
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-04-20 16:50:49 +0700

    Viết code xử lý
    1. Hàm đáp ứng sự kiện nhấn lên nút "Click Me".
    2. Xử lý chuyển màn hình.
    3. Tạo 1 đối tượng Intent.
    4. Tham số thứ 2 của hàm tạo này, là tên Activity (màn hình) ta muốn chuyển, hàm tạo có 2 tham số, tham số 2 là màn hình chuyển tới (.class) màn hình hiện tại, màn hình chuyển tới.
    5. Thực hiện chuyển, gửi thư, mà không đợi phản hồi.

--- IntentEx1/app/src/main/java/ntu/dinhvu61131562/intentex1/MainActivity.java
+++ IntentEx1/app/src/main/java/ntu/dinhvu61131562/intentex1/MainActivity.java
@@ -2,7 +2,9 @@ package ntu.dinhvu61131562.intentex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Change (View v){
+        Intent iScreen2 = new Intent(MainActivity.this, Surprise.class);
+        startActivity(iScreen2);
+
     }
 }
