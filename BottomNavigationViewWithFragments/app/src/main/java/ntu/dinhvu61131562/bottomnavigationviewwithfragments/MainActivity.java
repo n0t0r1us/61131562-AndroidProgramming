e0b408a664138e7301e1056453894779274d3995
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-04-21 08:43:40 +0700

    Navigation Item Selected Listener

--- BottomNavigationViewWithFragments/app/src/main/java/ntu/dinhvu61131562/bottomnavigationviewwithfragments/MainActivity.java
+++ BottomNavigationViewWithFragments/app/src/main/java/ntu/dinhvu61131562/bottomnavigationviewwithfragments/MainActivity.java
@@ -1,8 +1,12 @@
 package ntu.dinhvu61131562.bottomnavigationviewwithfragments;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottomNavView);
         frameLayout = findViewById(R.id.frameLayout);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.navHome){
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.add(R.id.frameLayout, new HomeFragment());
+                    fragmentTransaction.commit();
+                }else if(itemId == R.id.navShop){
+
+                }else { // nav Profile
+
+                }
+                return false;
+            }
+        });
     }
 }
