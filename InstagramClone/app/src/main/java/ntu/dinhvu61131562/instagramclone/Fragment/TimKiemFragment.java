135e8a68498b8ab66112f277a369ceae9843eb7b
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-05-21 19:42:09 +0700

    Update FragmentTimKiem.java
    Được sử dụng để thực hiện chức năng  cho phép người dùng tìm kiếm người dùng trong cơ sở dữ liệu Firebase và hiển thị kết quả trong một RecyclerView:
    1. Khai báo và khởi tạo biến:
    - recyclerView: hiển thị danh sách người dùng.
    - userAdapter: quản lý dữ liệu và hiển thị người dùng trong RecyclerView.
    - mUsers: Là một danh sách các đối tượng User, lưu trữ thông tin về người dùng.
    - thanh_TimKiem: Là một EditText để nhập nội dung tìm kiếm.
    2. onCreateView(): Phương thức này được gọi khi Fragment được tạo và hiển thị.
    - Layout của Fragment được gán bằng cách inflate từ file XML fragment_timkiem.
    - RecyclerView và EditText được ánh xạ từ layout XML.
    - RecyclerView được cấu hình với một LinearLayoutManager và gắn với Adapter.
    - Một TextWatcher được thiết lập cho EditText để theo dõi sự thay đổi trong nội dung nhập.
    3. timKiem(): Phương thức này thực hiện tìm kiếm người dùng trong cơ sở dữ liệu Firebase.
    - Sử dụng một Query để truy vấn cơ sở dữ liệu với điều kiện là tên người dùng bắt đầu hoặc kết thúc bằng nội dung tìm kiếm.
    - Khi dữ liệu được trả về từ cơ sở dữ liệu, danh sách người dùng được làm sạch và cập nhật, sau đó thông báo cho Adapter cập nhật giao diện.
    4. readUsers(): Phương thức này đọc tất cả người dùng từ cơ sở dữ liệu Firebase khi không có nội dung tìm kiếm.
    - Nếu EditText tìm kiếm trống, một truy vấn đơn giản được thực hiện để đọc tất cả người dùng từ cơ sở dữ liệu. Sau đó, danh sách người dùng được cập nhật và Adapter được thông báo để cập nhật giao diện.

--- InstagramClone/app/src/main/java/ntu/dinhvu61131562/instagramclone/Fragment/TimKiemFragment.java
+++ InstagramClone/app/src/main/java/ntu/dinhvu61131562/instagramclone/Fragment/TimKiemFragment.java
@@ -2,18 +2,113 @@ package ntu.dinhvu61131562.instagramclone.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ntu.dinhvu61131562.instagramclone.Adapter.UserAdapter;
+import ntu.dinhvu61131562.instagramclone.Model.User;
 import ntu.dinhvu61131562.instagramclone.R;
 public class TimKiemFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private UserAdapter userAdapter;
+    private List<User> mUsers;
+
+    EditText thanh_TimKiem;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timkiem, container, false);
+        View view = inflater.inflate(R.layout.fragment_timkiem, container, false);
+
+        recyclerView = view.findViewById(R.id.recycle_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        thanh_TimKiem = view.findViewById(R.id.thanh_TimKiem);
+
+        mUsers = new ArrayList<>();
+        userAdapter = new UserAdapter(getContext(), mUsers);
+        recyclerView.setAdapter(userAdapter);
+
+        readUsers();
+        thanh_TimKiem.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                timKiem(s.toString().toLowerCase());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return view;
+    }
+    private void timKiem(String s){
+        Query query = FirebaseDatabase.getInstance().getReference("Tài Khoản")
+                .orderByChild("taiKhoan")
+                .startAt(s)
+                .endAt(s+"\uf8ff");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mUsers.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    User user = dataSnapshot.getValue(User.class);
+                    mUsers.add(user);
+                }
+                userAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void readUsers(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Tài Khoản");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(thanh_TimKiem.getText().toString().equals("")){
+                    mUsers.clear();
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                        User user = dataSnapshot.getValue(User.class);
+                        mUsers.add(user);
+                    }
+                    userAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
