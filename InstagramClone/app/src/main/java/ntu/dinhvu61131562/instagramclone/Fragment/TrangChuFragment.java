33270d9ce961c6db7b25f77f0f923ffc5c0486f8
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-05-22 17:25:29 +0700

    Update FragmentTrangChu.java
    Hiển thị danh sách các bài đăng từ những người mà người dùng đang theo dõi. Sử dụng Firebase để lấy danh sách những người đang theo dõi và các bài đăng từ những người này, sau đó hiển thị chúng trong một RecyclerView sử dụng PostAdapter.

--- InstagramClone/app/src/main/java/ntu/dinhvu61131562/instagramclone/Fragment/TrangChuFragment.java
+++ InstagramClone/app/src/main/java/ntu/dinhvu61131562/instagramclone/Fragment/TrangChuFragment.java
@@ -2,19 +2,97 @@ package ntu.dinhvu61131562.instagramclone.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ntu.dinhvu61131562.instagramclone.Adapter.PostAdapter;
+import ntu.dinhvu61131562.instagramclone.Model.Post;
 import ntu.dinhvu61131562.instagramclone.R;
 
 public class TrangChuFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+    private List<Post> postLists;
+
+    private List<String> dangTheoDoiLists;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trangchu, container, false);
+        View view = inflater.inflate(R.layout.fragment_trangchu, container, false);
+
+        recyclerView = view.findViewById(R.id.recycle_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        postLists = new ArrayList<>();
+        postAdapter = new PostAdapter(getContext(), postLists);
+        recyclerView.setAdapter(postAdapter);
+
+        checkDangTheoDoi();
+        return view;
+    }
+
+    private void checkDangTheoDoi(){
+        dangTheoDoiLists = new ArrayList<>();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Theo Dõi")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child("Đang Theo Dõi");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                dangTheoDoiLists.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    dangTheoDoiLists.add(dataSnapshot.getKey());
+                }
+                readPosts();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void readPosts(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postLists.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Post post = dataSnapshot.getValue(Post.class);
+                    for (String id : dangTheoDoiLists){
+                        if(post.getNguoiDang().equals(id)){
+                            postLists.add(post);
+                        }
+                    }
+                }
+                postAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
