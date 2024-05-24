64085d38f486ea9ca70bc66d6e555527638520fa
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-05-24 14:23:50 +0700

    Update MyFotoAdapter.java
    Xử lý sự kiện của Adapter để hiển thị ảnh trong hồ sơ người dùng và xử lý sự kiện nhấn vào ảnh để hiển thị chi tiết bài đăng.

--- InstagramClone/app/src/main/java/ntu/dinhvu61131562/instagramclone/Adapter/MyFotoAdapter.java
+++ InstagramClone/app/src/main/java/ntu/dinhvu61131562/instagramclone/Adapter/MyFotoAdapter.java
@@ -1,18 +1,21 @@
 package ntu.dinhvu61131562.instagramclone.Adapter;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
 import java.util.List;
 
+import ntu.dinhvu61131562.instagramclone.Fragment.PostDetailFragment;
 import ntu.dinhvu61131562.instagramclone.Model.Post;
 import ntu.dinhvu61131562.instagramclone.R;
 
@@ -36,9 +39,22 @@ public class MyFotoAdapter extends RecyclerView.Adapter<MyFotoAdapter.ViewHolder
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-        Post post = mPosts.get(position);
+        final Post post = mPosts.get(position);
 
         Glide.with(context).load(post.getPostImage()).into(holder.post_image);
+
+        holder.post_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = context.getSharedPreferences("PREFS",
+                        Context.MODE_PRIVATE).edit();
+                editor.putString("postId", post.getPostId());
+                editor.apply();
+
+                ((FragmentActivity)context).getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frag_contain, new PostDetailFragment()).commit();
+            }
+        });
 
     }
 
