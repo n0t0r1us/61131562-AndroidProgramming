bd04f974f5fcbef040d351ecbb22e6b66176d07a
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-05-28 10:35:00 +0700

    Create NguoiTheoDoiActivity.java
    Hiển thị danh sách người dùng theo các tiêu chí khác nhau như "Lượt Thích", "Đang Theo Dõi", và "Người Theo Dõi". Dữ liệu được lấy từ Firebase Realtime Database.

---
+++ InstagramClone/app/src/main/java/ntu/dinhvu61131562/instagramclone/NguoiTheoDoiActivity.java
@@ -0,0 +1,167 @@
+package ntu.dinhvu61131562.instagramclone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ntu.dinhvu61131562.instagramclone.Adapter.UserAdapter;
+import ntu.dinhvu61131562.instagramclone.Model.User;
+
+public class NguoiTheoDoiActivity extends AppCompatActivity {
+
+    String id;
+    String tieuDe;
+
+    List<String> idList;
+
+    RecyclerView recyclerView;
+    UserAdapter userAdapter;
+    List<User> userList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nguoi_theo_doi);
+
+        Intent intent = getIntent();
+        id = intent.getStringExtra("id");
+        tieuDe = intent.getStringExtra("Tiêu Đề");
+
+        Toolbar toolbar = findViewById(R.id.toolBar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tieuDe);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        recyclerView = findViewById(R.id.recycle_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        userList = new ArrayList<>();
+        userAdapter = new UserAdapter(this, userList, false);
+        recyclerView.setAdapter(userAdapter);
+
+        idList = new ArrayList<>();
+
+        switch (tieuDe){
+            case "Lượt Thích":
+                getLuotThich();
+                break;
+            case "Đang Theo Dõi":
+                getDangTheoDoi();
+                break;
+            case "Người Theo Dõi":
+                getNguoiTheoDoi();
+                break;
+        }
+    }
+
+    private void getLuotThich() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Likes")
+                .child(id);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                idList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    idList.add(dataSnapshot.getKey());
+                }
+                showUsers();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getDangTheoDoi() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Theo Dõi")
+                .child(id)
+                .child("Đang Theo Dõi");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                idList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    idList.add(dataSnapshot.getKey());
+                }
+                showUsers();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getNguoiTheoDoi() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Theo Dõi")
+                .child(id)
+                .child("Người Theo Dõi");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                idList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    idList.add(dataSnapshot.getKey());
+                }
+                showUsers();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void showUsers(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Tài Khoản");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    User user = dataSnapshot.getValue(User.class);
+                    for (String id : idList){
+                        if (user.getId().equals(id)) {
+                            userList.add(user);
+                        }
+                    }
+                }
+                userAdapter.notifyDataSetChanged();
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
+
+
